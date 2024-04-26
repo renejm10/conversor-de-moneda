12a7338fa210b4dfa55b1a8df9f0710ffafd8bf0
Rene Jimenez <rene.jm10@gmail.com>
2024-04-26 09:04:10 -0600

    Se creo estructura del menu

--- src/com/renejm/conversormoneda/main/Main.java
+++ src/com/renejm/conversormoneda/main/Main.java
@@ -1,7 +1,29 @@
 package com.renejm.conversormoneda.main;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int op;
+        do{
+            System.out.println("*******************\n");
+            System.out.println("BIENVENIDO AL CONVERSOR DE MONEDA\n");
+            System.out.println("1) Dolar a Euro");
+            System.out.println("2) Euro a Dolar");
+            System.out.println("3) Lempira a Dolar");
+            System.out.println("4) Dolar a Lempira");
+            System.out.println("5) Lempira a Euro");
+            System.out.println("6) Euro a Lempira");
+            System.out.println("7) Salir");
+            System.out.println("********************\n");
+            System.out.print("Ingrese su opcion: ");
+            op = sc.nextInt();
+
+        }while(op!=7);
+
+
+
 
     }
 }
