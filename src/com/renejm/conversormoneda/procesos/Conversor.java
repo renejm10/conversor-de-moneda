aca2363029df76b7cb7a010ac9112fe8669850b5
Rene Jimenez <rene.jm10@gmail.com>
2024-04-26 10:23:39 -0600

    Se creo clase para calcular el cambio de moneda

---
+++ src/com/renejm/conversormoneda/procesos/Conversor.java
@@ -0,0 +1,9 @@
+package com.renejm.conversormoneda.procesos;
+
+public class Conversor {
+    public float conversor(float rate, float amount){
+        float respuesta = rate * amount;
+        return respuesta;
+    }
+
+}
